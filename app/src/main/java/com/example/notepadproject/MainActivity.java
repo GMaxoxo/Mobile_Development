98a401cfb305495d8a228980a45170650934c0e7
Maxoxo <gorkiy@maxoxo.ru>
2024-04-02 07:00:14 +0300

    Подготовка к работе с Firebase, ошибка не исправлена.

--- app/src/main/java/com/example/notepadproject/MainActivity.java
+++ app/src/main/java/com/example/notepadproject/MainActivity.java
@@ -14,6 +14,10 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.firebase.auth.FirebaseAuth;
+
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
 
     private EditText inputLoginET;
@@ -23,7 +27,10 @@ public class MainActivity extends AppCompatActivity {
     private Button registrationBtn;
 
     private Button authorizationBtn;
-    private TextView passVerification;
+    private TextView inputLoginAndPasswordTV;
+
+    private FirebaseAuth mAuth;
+
 
 
     @Override
@@ -31,58 +38,55 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
         // Инициализация
         init();
-        saveData();
+        // Обработчики нажатий
         setOnClickListeners();
 
-        // Добавляем обработчик для проверки пароля
-        inputPassET.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
-
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-                Proverka();
-            }
-
-            @Override
-            public void afterTextChanged(Editable s) {}
-        });
-
     }
+
 
 
     // Метод Инициализации
     private void init() {
+        inputLoginAndPasswordTV = findViewById(R.id.inputLoginAndPasswordTV);
         inputLoginET = findViewById(R.id.inputLoginET);
         inputPassET = findViewById(R.id.inputPassET);
         registrationBtn = findViewById(R.id.registrationBtn);
         authorizationBtn = findViewById(R.id.authorizationBtn);
-        passVerification = findViewById(R.id.passVerification);
+        mAuth = FirebaseAuth.getInstance();
     }
 
     // Метод обработчика нажатий
-    private void setOnClickListeners() {
+    private void setOnClickListeners(){
+        // Обработчк регистрации
+        registrationBtn.setOnClickListener((v -> {
+            String email = inputLoginET.getText().toString();
+            String pass = inputPassET.getText().toString();
+            if (!email.isEmpty() && !pass.isEmpty()) {
+                mAuth.signInWithEmailAndPassword(email, pass).addOnCompleteListener((it -> {
+                    if (it.isSuccessful()) {
+                        Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                        startActivity(intent);
+                        finish();
+                    } else {
+                        Toast.makeText(this, it.getException().toString(), Toast.LENGTH_SHORT).show();
 
-        String loginNotEmpty = inputLoginET.getText().toString();
-        String passwordNotEmpty = inputPassET.getText().toString();
-
-        // Проверяем, что и логин, и пароль не пустые
-        if (loginNotEmpty.isEmpty() || passwordNotEmpty.isEmpty()) {
-            return;
-        } else {
-            // Обработчк регистрации
-            registrationBtn.setOnClickListener((v -> saveData())
-            );
-        }
-
-
+                    }
+                }));
+            } else {
+                Toast.makeText(this, "Empty Fields Are not Allowed !!", Toast.LENGTH_SHORT).show();
+            }
+                })
+        );
         // Обработчк авторизации
-        authorizationBtn.setOnClickListener( v -> loadData() );
+        authorizationBtn.setOnClickListener((v -> {
+                    loadData();
+                })
+        );
 
     }
-
     // Метод сохранения
     private void saveData() {
         String login = inputLoginET.getText().toString();
@@ -108,62 +112,12 @@ public class MainActivity extends AppCompatActivity {
         if (inputLoginET.getText().toString().equals(login)  // перекидывает на новое окно
                 && inputPassET.getText().toString().equals(password)) {
             startActivity(new Intent(this, SecondActivity.class));
-            overridePendingTransition(0, 0); // Убирает анимки
         } else {        // выдаёт ошибку
-            Toast.makeText(this, "Error", Toast.LENGTH_LONG).show();
+            Toast.makeText(this, "Ошибка", Toast.LENGTH_LONG).show();
         }
-
-    }
-
-    private void Proverka() {
-
-        // Получаем пароль
-        String password = inputPassET.getText().toString();
-
-        // Флаги для проверки условий
-        boolean passCapital = false; // Заглавная буква
-        boolean passDigit = false; // Цифра
-        boolean passLength = false;  // Длинна
-        String passReport = "";
-
-        // Проверяем символы
-        for (int i = 0; i < password.length(); i++) {
-            char massivPassword = password.charAt(i);
-            if (Character.isUpperCase(massivPassword)) {
-                passCapital = true;
-            }
-
-            if (Character.isDigit(massivPassword)) {
-                passDigit = true;
-            }
-
-        }
-        // Проверяем длину пароля
-        if (password.length() > 7) {
-            passLength = true;
-        }
-
-        // Считаем пароль правильным
-        if (passDigit && passCapital && passLength) {
-            passVerification.setText("Правильный пароль");
-            passReport += "Хороший пароль";
-            saveData();
-            setOnClickListeners();
-        } else {
-
-            // Отчёт об ошибке
-            passReport += "Пароль должен содержать минимум 8 символов. Также он должен содержать как минимум одну цифру и заглавную букву";
-            failregistration();
-        }
-
-
-        passVerification.setText(passReport);
+        //  inputLoginAndPasswordTV.setText(login + " " + password);  // как-будто можно убрать уже
     }
 
 
-    // Показ неудачной регистрации
-    private void failregistration() {
-        registrationBtn.setOnClickListener( v -> Toast.makeText(getApplicationContext(), "Пароль не введён по требованиям", Toast.LENGTH_SHORT).show() );
-    }
 
 }
